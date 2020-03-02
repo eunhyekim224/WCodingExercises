c3407b10e5f743bfe25ea01b30f569cb563b6435
kimeunhye224 <kimeunhye224@gmail.com>
2020-03-02 13:55:37 +0200

    update minichat exercise

--- PHP/MySQL/minichat/chat.php
+++ PHP/MySQL/minichat/chat.php
@@ -49,37 +49,7 @@
             </form>
             <div id="log">
                 <?php
-                        try {
-                            $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '',
-                            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-                        } catch(Exception $e) {
-                            die('Error: '. $e->getMessage());
-                        }
-
-                        if (isset($_POST['currentMsgCount'])) {
-                            $messagecount = $_POST['currentMsgCount'] + 10;
-                        } else {
-                            $messagecount = 10;
-                        }
-
-                        $totalMessages =  $db->query("SELECT COUNT(ID) as msg_count FROM minichat");
-                        $totalMessagescount = $totalMessages->fetch()['msg_count'];
-
-                        $startPosition = $totalMessagescount - $messagecount;
-
-                        if ($startPosition > 0) {
-                            $convo = $db->query("SELECT * FROM minichat ORDER BY ID LIMIT $startPosition, $messagecount");
-                            while ($data = $convo->fetch()) {
-                                    echo '<li>'. $data['pseudo']. ': '. $data['message']. '</li>';                                             
-                            } 
-                            $convo->closeCursor();
-                        } else {
-                            $convo = $db->query("SELECT * FROM minichat ORDER BY ID LIMIT 0, $messagecount");
-                            while ($data = $convo->fetch()) {
-                                    echo '<li>'. $data['pseudo']. ': '. $data['message']. '</li>';                                             
-                            } 
-                            $convo->closeCursor();          
-                        }        
+                    include('display_old_msg.php');
                 ?>
             </div>
             <div id="oldLog"> 
--- PHP/MySQL/minichat/chat_post.php
+++ PHP/MySQL/minichat/chat_post.php
@@ -7,18 +7,16 @@
     }
                 
     if ($_POST['pseudo'] !== '' AND $_POST['message'] !== '') {
-        $response = $db->prepare("INSERT INTO minichat(pseudo, message) VALUES(:pseudo, :message)");
+        $response = $db->prepare("INSERT INTO minichat(pseudo, message, date_creation) VALUES(:pseudo, :message, :date_creation)");
         $response->execute(array(
             'pseudo' => htmlspecialchars($_POST['pseudo']),
-            'message' =>  htmlspecialchars($_POST['message'])
+            'message' =>  htmlspecialchars($_POST['message']),
+            'date_creation' => date('y-m-d H:i:s')
         ));
         $response->closeCursor();
 
         setcookie('pseudo', $_POST['pseudo']);
-
-        header('Location: chat.php');    
-    } else {
-        header('Location: chat.php');    
     }
+        header('Location: chat.php');    
   
 ?>
